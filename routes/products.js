963ff99493ba284df4a8a9ac5fad5530fb3e2889
何柯 <heke@hujiang.com>
2016-04-11 12:07:51 +0800

    清理原框架代码

--- routes/products.js
+++
@@ -1,6 +0,0 @@
-exports.list=function(req,res,next){
-    req.collections.products.find({}).toArray(function(error, products) {
-        if (error) return next(error);
-        res.send({products:products});
-    });
-};
