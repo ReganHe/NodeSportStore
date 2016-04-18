4d474c89779f9f4ef9158629dab58905f93278ed
ReganHe <heoo547627@126.com>
2016-04-18 23:08:02 +0800

    提供数据查询信息

--- client/app/flux/ProductStore.js
+++ client/app/flux/ProductStore.js
@@ -5,15 +5,26 @@ var CHANGE_EVENT = "changeEvent";
 var ProductStore = assign({}, EventEmitter.prototype, {
 // Survey-specific methods
     listProducts: function () {
-        return [
-            {
-                productId: 123,
-                name: 'asd123',
-                description: 'Superhero mashup',
-                category: 'ad123',
-                prize:88.88
-            }
-        ];
+      var  mongoskin = require('mongoskin'),
+            dbUrl = process.env.MONGOHQ_URL || 'mongodb://@localhost:27017/sportStore',
+            db = mongoskin.db(dbUrl, {safe: true}),
+            collections = {
+                products: db.collection('products')
+            };
+        collections.products.find({published: true}, {sort: {_id:-1}}).toArray(function(error, products) {
+            if (error) return next(error);
+            return products;
+        }
+
+        //return [
+        //    {
+        //        productId: 123,
+        //        name: 'asd123',
+        //        description: 'Superhero mashup',
+        //        category: 'ad123',
+        //        prize:88.88
+        //    }
+        //];
     },
 
 // Basic event handling functions
